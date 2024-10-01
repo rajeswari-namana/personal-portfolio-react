04bdbcd05aaf1b8b1c3ea5a3cbea7b138a232ac7
Rajeswari Namana <raji.namana@gmail.com>
2024-10-01 08:18:03 -0500

    basic setup

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hero.css';
+import profile_img from'./profile_img';
+
+const Hero = () => {
+  return (
+    <div><image src={profile_img} alt=''/></div>
+  )
+}
+
+export default Hero
