675dae90cc709eff1317480a07fe1858d4ae4f26
Rajeswari Namana <raji.namana@gmail.com>
2024-10-01 08:43:47 -0500

    testing

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Hero.css';
-import profile_img from'./profile_img';
+import profile_img from '../../assets/profileImg.jpg';
 
 const Hero = () => {
   return (
-    <div><image src={profile_img} alt=''/></div>
+    <div>
+      <image src={profile_img} alt=''/>
+      <h1>Hi, I am Raji, frontend developer based in USA.</h1>
+      <p>Hi, I am Raji, frontend developer based in USA.</p>
+      </div>
   )
 }
 
