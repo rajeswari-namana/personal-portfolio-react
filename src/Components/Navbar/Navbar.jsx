1a8001e886d4b518b52ddb4e570a80dc5c73f894
Rajeswari Namana <raji.namana@gmail.com>
2024-10-10 13:55:05 -0500

    sticky navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,56 @@
-import React from 'react'
+import React from 'react';
 import './Navbar.css'
 
-const Navbar = () => {
-    return (
-        <div className='navbar' >
-            <h1>Raji</h1>
-            <ul className="nav-menu">
-                <li>
-                    <a href="#home">Home</a>
-                </li>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#skills">Skills</a>
-                </li>
-                <li>
-                    <a href="#experience">Experience</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
-                </li>
-            </ul>
-            <div className="nav-connect">
-                <a href="#connect">Connect</a>
-            </div>
-        </div>
-    )
-}
+// function useScrollTop() {
+//     const [scrollTop, setScrollTop] = useState(0);
 
-export default Navbar
+//         const handleScroll = () => {
+//             setScrollTop(window.scrollY);
+//         };
+
+//         useEffect(() => {
+//             window.addEventListener('scroll', handleScroll);
+//             return () => {
+//                 window.removeEventListener('scroll', handleScroll);
+//             };
+//         }, []);
+
+//         return scrollTop;
+//     }
+
+    const Navbar = () => {
+        return (
+            <div
+                className='navbar-container'
+            // style={{
+            //     borderBottom: scrollTop > 0 ? "1px solid #f5ebe0" : "none",
+            // }}
+            >
+                <div className='navbar' >
+                    <h1>Raji</h1>
+                    <ul className="nav-menu">
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#skills">Skills</a>
+                        </li>
+                        <li>
+                            <a href="#experience">Experience</a>
+                        </li>
+                        <li>
+                            <a href="#projects">Projects</a>
+                        </li>
+                    </ul>
+                    <div className="nav-connect">
+                        <a href="#connect">Connect</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    export default Navbar;
